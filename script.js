1277996eb518c01e80ac12cc45ea55b4d7119045
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-10-07 21:38:26 +0530

    learned tabbed javascript component

--- script.js
+++ script.js
@@ -32,17 +32,17 @@ document.addEventListener('keydown', function (e) {
 ///////////////////////////////////////
 
 const allSelections = document.querySelectorAll('.section');
-console.log(document.getElementById('section--1'));
-console.log(allSelections);
-console.log(document.getElementsByTagName('button'));
+// console.log(document.getElementById('section--1'));
+// console.log(allSelections);
+// console.log(document.getElementsByTagName('button'));
 
-console.log(document.getElementsByClassName('btn'));
+// console.log(document.getElementsByClassName('btn'));
 
 // Creating and inserting a new element
 
 const message = document.createElement('div');
 const header = document.querySelector('.header');
-console.log(message);
+// console.log(message);
 
 message.classList.add('cookie-message');
 message.textContent = 'We use cookies for improved functionality and anylytics';
@@ -67,16 +67,16 @@ document.documentElement.style.setProperty('--color-primary', '');
 const logo = document.querySelector('.nav__logo');
 logo.alt = 'Beautiful minimalist logo';
 
-console.log(logo.getAttribute('alt'));
+// console.log(logo.getAttribute('alt'));
 
 logo.getAttribute('src');
 
 const link = document.querySelector('.nav__link--btn');
 
-console.log(link.href);
-console.log(link.getAttribute('href'));
+// console.log(link.href);
+// console.log(link.getAttribute('href'));
 
-console.log(link.dataset);
+// console.log(link.dataset);
 
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 
@@ -84,13 +84,13 @@ const section1 = document.querySelector('#section--1');
 
 btnScrollTo.addEventListener('click', e => {
   const s1coords = section1.getBoundingClientRect();
-  console.log(e.target.getBoundingClientRect());
-  console.log('Current scroll position: ' + window.pageXOffset, pageYOffset);
+  //   console.log(e.target.getBoundingClientRect());
+  //   console.log('Current scroll position: ' + window.pageXOffset, pageYOffset);
 
-  console.log(
-    'height/width of viewport: ' + document.documentElement.clientHeight,
-    document.documentElement.clientWidth
-  );
+  //   console.log(
+  //     'height/width of viewport: ' + document.documentElement.clientHeight,
+  //     document.documentElement.clientWidth
+  //   );
   //scrolling
 
   //   window.scrollTo(
@@ -107,8 +107,6 @@ btnScrollTo.addEventListener('click', e => {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
 
-const h1 = document.querySelector('h1');
-
 // const alertH1 = function (e) {
 //   alert('addeventlistener: great you are reading this message');
 // };
@@ -121,25 +119,94 @@ const h1 = document.querySelector('h1');
 
 /// rgba(255, 255, 255);
 
-const randomInt = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+// const randomInt = (min, max) => {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// };
 
-const randomColor = () => {
-  return `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(
-    0,
-    255
-  )})`;
-};
+// const randomColor = () => {
+//   return `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(
+//     0,
+//     255
+//   )})`;
+// };
 
-document.querySelector('.nav__link').addEventListener('click', function (e) {
-  this.style.backgroundColor = randomColor();
-});
+// document.querySelector('.nav__link').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+// });
+
+// document.querySelector('.nav__links').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+// });
+
+// document.querySelector('.nav').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+// });
+
+// document.querySelectorAll('.nav__link').forEach(link => {
+//   link.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
 
 document.querySelector('.nav__links').addEventListener('click', function (e) {
-  this.style.backgroundColor = randomColor();
+  e.preventDefault();
+  //Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
 });
 
-document.querySelector('.nav').addEventListener('click', function (e) {
-  this.style.backgroundColor = randomColor();
+// const h1 = document.querySelector('h1');
+
+// console.log(h1.querySelectorAll('.highlight'));
+// console.log(h1.childNodes);
+
+// h1.firstElementChild.style.color = 'white';
+// h1.lastElementChild.style.color = 'orangered';
+
+// //Going upwards: parents
+
+// console.log(h1.parentNode);
+// console.log(h1.parentElement);
+
+// h1.closest('.header').style.background = 'var(--gradient-secondary)';
+
+// //Going sideways: siblings
+
+// console.log(h1.previousElementSibling);
+// console.log(h1.nextElementSibling);
+
+// console.log(h1.previousSibling);
+// console.log(h1.nextSibling);
+
+// console.log(h1.parentElement.children);
+// [...h1.parentElement.children].forEach(child => {
+//   if (child !== h1) child.style.transform = 'scale(0.5)';
+// });
+
+//Tabbed component
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  if (!clicked) return;
+  //Active Tab
+  tabs.forEach(tab => tab.classList.remove('operations__tab--active'));
+
+  clicked.classList.add('operations__tab--active');
+
+  //Active Content area
+  tabsContent.forEach(tab =>
+    tab.classList.remove('operations__content--active')
+  );
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
 });
